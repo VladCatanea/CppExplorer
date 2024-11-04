bc13fc3e84aa6777ab14f09466e190516f48a845
Vlad <vlad.catanea@yahoo.com>
2024-11-04 21:49:19 +0200

    started adding reading the student list

--- main.cpp
+++ main.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+#define FILENAME "students.txt"
+#define MAX_STUDENTS 50
 
 using namespace std;
 
+class StudentList{
+    private:
+    char* students[MAX_STUDENTS];
 
-// comment
+    public:
+    void init(string filename){
+        ifstream file (filename);
+        string buffer;
+        char* student_name;
+        int i = 0;
+
+        cout << "File open success?: " << file.is_open() << endl;
+
+        if(file.is_open()){
+
+            while(getline(file, buffer) && i < MAX_STUDENTS){
+                cout << "Reading line " << i << endl;
+                student_name = new char[buffer.size()];
+                buffer.copy(student_name, buffer.size());
+                students[i] = student_name;
+                i++;
+            }
+            cout << "end: " << i << endl;
+            students[i] = NULL;
+            file.close();
+        }
+        else{
+            cout << "ERROR: Couldn't open file \"" << filename << "\"" << endl;
+        }
+    };
+
+    void print(){
+        cout << students[0];
+        for(int i = 0; students[i] != NULL; i++){
+            cout << students[i] << endl;
+        }
+    };
+};
+
 
 int main()
 {
-    cout << "Hello World" << endl;
+    cout << endl;
+    
+    StudentList student_list = StudentList();
+    student_list.init(FILENAME);
+    student_list.print();
+
+    return 0;
 }
